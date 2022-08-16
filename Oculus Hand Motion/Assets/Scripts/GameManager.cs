8702d4eb4c3d24a04f900925449e55881a24cf9a
SHLee17 <62954002+SHLee17@users.noreply.github.com>
2022-08-16 17:58:44 +0900

    Add Cube

--- Oculus Hand Motion/Assets/Scripts/GameManager.cs
+++ Oculus Hand Motion/Assets/Scripts/GameManager.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class GameManager : MonoBehaviour
 {
-
+    public TMP_Text debug;
+    public GameObject obj;
 
     void Start()
     {
-
+        debug.text = obj.name;
     }
 
     void Update()
