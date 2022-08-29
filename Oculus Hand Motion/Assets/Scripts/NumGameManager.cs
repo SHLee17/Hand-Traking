abe2f424c80071adb9005aa4394becee9968e188
SHLee17 <62954002+SHLee17@users.noreply.github.com>
2022-08-29 18:04:29 +0900

    Update

--- Oculus Hand Motion/Assets/Scripts/NumGameManager.cs
+++ Oculus Hand Motion/Assets/Scripts/NumGameManager.cs
@@ -8,38 +8,68 @@ public class NumGameManager : MonoBehaviour
 {
     enum Order
     {
-        first = 1,
-        second,
-        third
+        First,
+        Second,
+        Third
+    }
+    enum Sign
+    {
+        Add,
+        Subtract,
+        Divide,
+        Multiply
     }
 
     [System.Serializable]
     public class Number
     {
         public TMP_Text txtNum;
-        public int num;
+        int num;
+
+        public int Num
+        {
+            get => num;
+            set
+            {
+                num = value;
+                txtNum.text = num.ToString();
+            }
+        }
     }
 
+    [Header("Arrays")]
     public Transform[] fingerPoses;
-    public List<Pose> poseList;
-    public List<Number> numList;
+    public Number[] nums;
+    [SerializeField]
+    List<Pose> poseList;
 
-    [Header("Text")]
+    [Header("Texts")]
     public TMP_Text[] txtDirections;
     public TMP_Text txtTimer;
     public TMP_Text txtSign;
 
-    Order order;
+    [Header("Enums")]
+    [SerializeField]
+    Sign sign;
+    [SerializeField]
+    Order blankOrder;
+
+    [Header("Data Type")]
     int leftCount, rightCount;
-    Dictionary<Order, Number> answerDict;
     float timer;
+    [SerializeField]
+    int answer;
+    [SerializeField]
     bool isSetExamQuestions;
+    Dictionary<Order, Number> answerDict;
+
 
     void Start()
     {
-        answerDict.Add(Order.first, numList[0]);
-        answerDict.Add(Order.second, numList[1]);
-        answerDict.Add(Order.third, numList[2]);
+        answerDict = new Dictionary<Order, Number>();
+        answerDict.Add(Order.First, nums[0]);
+        answerDict.Add(Order.Second, nums[1]);
+        answerDict.Add(Order.Third, nums[2]);
 
         timer = 30;
         foreach (Transform item in fingerPoses)
@@ -51,45 +81,128 @@ public class NumGameManager : MonoBehaviour
 
     void Update()
     {
-        if(!isSetExamQuestions)
+        if (!isSetExamQuestions)
         {
-            order = (Order)RandRange(1,4);
+            blankOrder = RandomEnum<Order>(1);
+            sign = RandomEnum<Sign>();
 
-            switch (order)
+            switch (sign)
             {
-                case Order.first:
-
+                case Sign.Add:
+                    txtSign.text = "+";
                     break;
-                case Order.second:
+                case Sign.Subtract:
+                    txtSign.text = "-";
                     break;
-                case Order.third:
+                case Sign.Divide:
+                    txtSign.text = "¡À";
+                    break;
+                case Sign.Multiply:
+                    txtSign.text = "x";
                     break;
             }
 
+            ExampleQuestion(sign, blankOrder);
             isSetExamQuestions = true;
         }
-
         if (timer > 0)
         {
             timer -= Time.deltaTime;
             txtTimer.text = timer.ToString("N1");
-            
+
+            CheckCount();
         }
 
-        CheckCount();
     }
-
-    int RandRange(int i, int j)
+    void ExampleQuestion(Sign sign, Order blankOrder)
     {
-        return Random.Range(i, j); ;
+        answerDict[blankOrder].Num = 0;
+
+        void NumSet(Order order, int randNum) => answerDict[order].Num = randNum;
+        int NumGet(Order order) { return answerDict[order].Num; }
+
+        switch (sign)
+        {
+            case Sign.Add:
+                if (blankOrder == Order.Second)
+                {
+                    NumSet(Order.First, RandRange(1, 10));
+                    NumSet(Order.Third, RandRange(NumGet(Order.First) + 1, 20));
+                    answer = NumGet(Order.Third) - NumGet(Order.First);
+                }
+                else
+                {
+                    NumSet(Order.First, RandRange(1, 9));
+                    answer = RandRange(NumGet(Order.First) + 1, 10);
+                    NumSet(Order.Second, answer - NumGet(Order.First));
+                }
+                break;
+
+            case Sign.Subtract:
+                if (blankOrder == Order.Second)
+                {
+                    NumSet(Order.First, RandRange(1, 100));
+                    answer = RandRange(1, 10);
+                    NumSet(Order.Third, NumGet(Order.First) - answer);
+                }
+                else
+                {
+                    answer = RandRange(1, 10);
+                    NumSet(Order.First, RandRange(1, 100));
+                    NumSet(Order.Second, NumGet(Order.First) - answer);
+                }
+                break;
+
+            case Sign.Divide:
+                if (blankOrder == Order.Second)
+                {
+                    NumSet(Order.Third, RandRange(1, 9));
+                    answer = RandRange(1, 10);
+                    NumSet(Order.First, NumGet(Order.Third) * answer);
+
+                }
+                else
+                {
+                    answer = RandRange(1, 10);
+                    NumSet(Order.Second, RandRange(1, 9));
+                    NumSet(Order.First, NumGet(Order.Second) * answer);
+                }
+                break;
+
+            case Sign.Multiply:
+                if (blankOrder == Order.Second)
+                {
+                    answer = RandRange(1, 10);
+                    NumSet(Order.First, RandRange(1, 19));
+                    NumSet(Order.Third, answer * NumGet(Order.First));
+                }
+                else
+                {
+                    answer = RandRange(1, 10);
+                    NumSet(Order.First, RandRange(1, answer));
+                    NumSet(Order.Second, answer / NumGet(Order.First));
+                }
+                    break;
+        }
+
+        Debug.Log(answer);
+    }
+    T RandomEnum<T>(int min = 0)
+    {
+        System.Array values = System.Enum.GetValues(typeof(T));
+        return (T)values.GetValue(new System.Random().Next(min, values.Length));
+    }
+    int RandRange(int min, int max)
+    {
+        return Random.Range(min, max + 1);
     }
     void CheckCount()
     {
         foreach (Pose item in poseList)
         {
-            if(item.dir == Direction.Left)
+            if (item.dir == Direction.Left)
             {
-                if(item.select)
+                if (item.select)
                     leftCount = item.num;
             }
             else
@@ -104,14 +217,13 @@ public class NumGameManager : MonoBehaviour
 
         string answer = (leftCount + rightCount).ToString();
 
-        switch (order)
+        switch (blankOrder)
         {
-            case Order.first:
-
+            case Order.First:
                 break;
-            case Order.second:
+            case Order.Second:
                 break;
-            case Order.third:
+            case Order.Third:
                 break;
         }
 
