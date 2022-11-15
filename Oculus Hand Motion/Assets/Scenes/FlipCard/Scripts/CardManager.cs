f5661f7d920f14d70d273b524a3af853a7c4a256
rlatkddn83 <84095116+rlatkddn83@users.noreply.github.com>
2022-11-15 11:51:13 +0900

    update 22-11-15

---
+++ Oculus Hand Motion/Assets/Scenes/FlipCard/Scripts/CardManager.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using UnityEngine;
+
+public class CardManager : MonoBehaviour
+{
+    public FlipCardManager flipCardManager;
+    public GameObject thisCard;
+    public Animator flipCardAnim;
+    public CardSpriteList thisCardImage;
+    public int cardPos;
+    public int cardNum;
+
+    bool checkPair;
+    
+
+    private void Start()
+    {
+        flipCardManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<FlipCardManager>();
+
+        checkPair = false;
+    }
+
+    public void PickTheCard()
+    {
+        if (GetStatus())
+            return;
+
+        if (flipCardManager.processing)
+            return;
+
+        FlipCard(true);
+
+        if (flipCardManager.picked)
+        {
+            checkPair = flipCardManager.pickedCard.cardNum == cardNum;
+            if (checkPair)
+            {
+                flipCardManager.Judge();
+            }
+            else
+            {
+                StartCoroutine(WaitAndFlip(1.5f));
+            }
+
+            flipCardManager.picked = false;
+        }
+        else
+        {
+            flipCardManager.pickedCard = this;
+            flipCardManager.picked = true;
+        }
+
+        StartCoroutine(progressing(1.5f));
+    }
+
+    IEnumerator WaitAndFlip(float i)
+    {
+        yield return new WaitForSeconds(i);
+        FlipCard(false);
+        flipCardManager.FlipThatCard(flipCardManager.pickedCard, false);
+    }
+
+    IEnumerator progressing(float i)
+    {
+        flipCardManager.processing = true;
+        yield return new WaitForSeconds(i);
+        flipCardManager.processing = false;
+    }
+
+    public void FlipCard(bool setCard)
+    {
+        flipCardAnim.SetBool("Selected", setCard);
+    }
+
+    bool GetStatus()
+    {
+        return flipCardAnim.GetBool("Selected");
+    }
+}
